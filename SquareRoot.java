00bd5f7ea9dbcdedaf860a0edfefe0a68912fc41
KHUBAIB NAEEM <114779652+KHUBAIBNAEEM@users.noreply.github.com>
2024-12-18 15:23:13 +0500

    Add files via upload

---
+++ SquareRoot.java
@@ -0,0 +1,11 @@
+// Implement a program to calculate the square root using Math class.
+
+
+public class SquareRoot {
+        public static void main(String[] args) {
+            double num = 3;
+            double SquareRoot = Math.sqrt(num);
+            System.out.println("Square root: " + SquareRoot);
+        }
+    }
+    
