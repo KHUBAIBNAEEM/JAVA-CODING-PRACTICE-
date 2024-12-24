20d0e9ca5c7b39f489515ffe22b88ccf6041fd37
KHUBAIB NAEEM <114779652+KHUBAIBNAEEM@users.noreply.github.com>
2024-12-25 03:50:40 +0500

    Add files via upload

---
+++ CastingWrapperToInt.java
@@ -0,0 +1,9 @@
+public class CastingWrapperToInt {
+    public static void main(String[] args) {
+        Integer integerObject = 11;
+        int intValue = integerObject; // Auto-unboxing
+
+        System.out.println("Integer object: " + integerObject);
+        System.out.println("Int value: " + intValue);
+    }
+}
